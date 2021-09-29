876e65186dbfc67574e6df99ab8151d6c03c476d
ChloeKonde <konde.chloe@gmail.com>
2021-09-30 02:04:31 +0600

    add new component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <TagsComponent v-bind:tags="chips" justify="start"/>
+      <TagsComponent v-bind:tags="chips" justify="center"/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TagsComponent from './components/TagsComponent.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    TagsComponent,
+  },
+
+  data() {
+    return {
+      chips: [
+        {title: "first", icon:"mdi-fire"},
+        {title: "second", icon:"mdi-food-apple"},
+        {title: "third", icon:"mdi-fish"},
+        {title: "fourth", icon:"mdi-eye"},
+        {title: "fifth", icon:"mdi-donkey"},
+        {title: "sixth", icon:"mdi-dna"},
+        {title: "seventh", icon:"mdi-dna"},
+      ]
+    }
+  },
+};
+</script>
---
+++ src/components/TagsComponent.vue
@@ -0,0 +1,42 @@
+<template>
+<v-container>
+ <v-row :justify="justify">
+    <v-col
+      cols="2"
+      sm="1"
+      md="6"
+      lg="5">
+
+        <div>
+          <v-chip-group
+            column>
+
+            <v-chip outlined
+              v-for="tag in tags"
+              :key="tag">
+              
+              <v-icon left>
+                {{ tag.icon }}
+              </v-icon>
+
+              {{ tag.title }}
+              
+            </v-chip>
+
+            
+          </v-chip-group>
+        </div>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+  export default {
+    name: 'TagsComponent',
+    props: ['tags', 'justify']
+    }
+</script>
